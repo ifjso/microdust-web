63d9d81490f681b118817232838fa873dd976496
ifjso <ojsboy@hotmail.com>
2019-11-17 15:37:09 +0900

    center 맞추기

--- src/maps/SimpleMap.js
+++ src/maps/SimpleMap.js
@@ -1,12 +1,34 @@
 import React from 'react';
 import GoogleMap from './GoogleMap';
 
-const AnyReactComponent = ({ text }) => <div>{text}</div>;
-
-const SimpleMap = ({ center = { lat: 37.545, lng: 126.992 }, zoom = 7 }) => (
-  <GoogleMap defaultCenter={center} defaultZoom={zoom}>
-    <AnyReactComponent lat={37.545} lng={126.992} text="My Marker" />
-  </GoogleMap>
+const SimpleMap = ({
+  center = { lat: 35.993201, lng: 127.759226 },
+  zoom = 7
+}) => (
+  <GoogleMap
+    defaultCenter={center}
+    defaultZoom={zoom}
+    options={{
+      restriction: {
+        latLngBounds: {
+          north: 43.522419,
+          south: 28.291993,
+          west: 116.637253,
+          east: 138.346238
+        },
+        strictBounds: true
+      }
+    }}
+    yesIWantToUseGoogleMapApiInternals
+    onGoogleApiLoaded={({ map, maps }) => handleApiLoaded(map, maps)}
+  />
 );
+
+const handleApiLoaded = (map, maps) => {
+  loadMapShapes(map);
+};
+
+const loadMapShapes = map =>
+  map.data.loadGeoJson('http://localhost:3000/TL_SCCO_CTPRVN.geojson');
 
 export default SimpleMap;
