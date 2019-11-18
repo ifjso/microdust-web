aa6508434b0442447779a274da5ee50875df8ed7
ifjso <ojsboy@hotmail.com>
2019-11-18 20:50:41 +0900

    랜덤 컬러 삭제

--- src/maps/ShapeMap.js
+++ src/maps/ShapeMap.js
@@ -21,20 +21,17 @@ const handleApiLoaded = (map, maps) => {
   loadMapShapes(map);
 
   map.data.setStyle(feature => {
-    const color = feature.getProperty('weight') > 50 ? '#fff' : '#000';
-
     return {
       strokeColor: '#fff',
       strokeWeight: 1,
-      cursor: 'url(https://maps.gstatic.com/mapfiles/openhand_8_8.cur)',
-      fillColor: color
+      cursor: 'url(https://maps.gstatic.com/mapfiles/openhand_8_8.cur)'
     };
   });
 
   map.data.addListener('mouseover', e => {
     map.data.overrideStyle(e.feature, {
       fillColor: '#0077ff',
-      fillOpacity: 0.7
+      fillOpacity: 0.2
     });
   });
 
