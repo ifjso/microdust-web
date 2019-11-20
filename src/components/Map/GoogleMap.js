ce36e91cf9cd20d4997166ff9e9aa00a7e1ab352
ifjso <ojsboy@hotmail.com>
2019-11-20 09:10:38 +0900

    파일 패스 수정

--- src/components/Map/GoogleMap.js
+++ src/components/Map/GoogleMap.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import GoogleMapReact from 'google-map-react';
-import { GOOGLE_API_KEY } from '../config/googleApiKey';
+import { GOOGLE_API_KEY } from '../../config/googleApiKey';
 
 const GoogleMap = ({ children, ...props }) => (
   <div style={{ height: '100vh', width: '100%' }}>
