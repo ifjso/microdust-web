6734c53dd00d483844dde60c65b2b184ced4d64b
ifjso <ojsboy@hotmail.com>
2019-11-24 19:57:33 +0900

    map data load 방식 변경

--- src/components/Map/ShapeMap.js
+++ src/components/Map/ShapeMap.js
@@ -2,6 +2,9 @@ import React from 'react';
 import SimpleMap from './SimpleMap';
 import CTPRVN from '../../data/CTPRVN';
 import SIG from '../../data/SIG';
+import { getCity } from '../../utils/helper/cities-helper';
+import { getColor, getOpacity } from '../../utils/helper/color-helper';
+import { getMicrodustsByCity } from '../../modules/api/airkorea-api';
 
 const ShapeMap = ({
   center = { lat: 35.993201, lng: 127.759226 },
@@ -9,24 +12,7 @@ const ShapeMap = ({
   options,
   children
 }) => {
-  const onLoad = (map, maps) => {
-    const data1 = loadMapShapes(map, maps, CTPRVN);
-    const data2 = loadMapShapes(map, maps, SIG);
-
-    data1.setMap(map);
-    data2.setMap(null);
-
-    map.addListener('zoom_changed', () => {
-      const zoom = map.getZoom();
-      if (zoom > 8) {
-        data1.setMap(null);
-        data2.setMap(map);
-      } else {
-        data1.setMap(map);
-        data2.setMap(null);
-      }
-    });
-  };
+  const onLoad = (map, maps) => loadMapShapes(map, maps);
 
   return (
     <SimpleMap center={center} zoom={zoom} options={options} onLoad={onLoad}>
@@ -35,31 +21,70 @@ const ShapeMap = ({
   );
 };
 
-const loadMapShapes = (map, maps, geoData) => {
-  const data = new maps.Data({ map });
+const loadMapShapes = async (map, maps) => {
+  const data1 = new maps.Data({ map });
+  const data2 = new maps.Data({ map });
 
-  data.addGeoJson(geoData);
+  data1.addGeoJson(CTPRVN);
+  data2.addGeoJson(SIG);
 
-  data.setStyle(feature => {
+  const data = await getMicrodustsByCity();
+
+  data1.forEach(feature => {
+    const city = getCity(feature.getProperty('CTPRVN_CD'));
+    feature.setProperty('value', data[0][city]);
+  });
+
+  data2.setMap(null);
+
+  data1.setStyle(feature => {
+    console.log(1);
+
     return {
       strokeColor: '#fff',
       strokeWeight: 1,
-      cursor: 'url(https://maps.gstatic.com/mapfiles/openhand_8_8.cur)'
+      cursor: 'url(https://maps.gstatic.com/mapfiles/openhand_8_8.cur)',
+      // fillColor: getColor(feature.getProperty('value')),
+      fillColor: '#000',
+      fillOpacity: getOpacity(feature.getProperty('value'))
     };
   });
 
-  data.addListener('mouseover', e => {
-    data.overrideStyle(e.feature, {
-      fillColor: '#0077ff',
-      fillOpacity: 0.2
-    });
+  data2.setStyle(feature => {
+    console.log(2);
+
+    return {
+      strokeColor: '#fff',
+      strokeWeight: 1,
+      cursor: 'url(https://maps.gstatic.com/mapfiles/openhand_8_8.cur)',
+      fillColor: '#000',
+      fillOpacity: 0.5
+    };
   });
 
-  data.addListener('mouseout', e => {
-    data.revertStyle();
+  // data1.setMap(map);
+
+  map.addListener('zoom_changed', () => {
+    const zoom = map.getZoom();
+    if (zoom > 7) {
+      data1.setMap(null);
+      data2.setMap(map);
+    } else {
+      data1.setMap(map);
+      data2.setMap(null);
+    }
   });
 
-  return data;
+  // data.addListener('mouseover', e => {
+  //   data.overrideStyle(e.feature, {
+  //     fillColor: '#0077ff',
+  //     fillOpacity: 0.2
+  //   });
+  // });
+
+  // data.addListener('mouseout', e => {
+  //   data.revertStyle();
+  // });
 };
 
 export default ShapeMap;
