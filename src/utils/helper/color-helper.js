b915be4647e5e27152fe7f1946c2fbcd2e8c4c86
ifjso <ojsboy@hotmail.com>
2019-12-11 04:55:59 +0900

    color, opacity 수정

--- src/utils/helper/color-helper.js
+++ src/utils/helper/color-helper.js
@@ -1,33 +1,36 @@
+const MAX_COLOR = 255;
+const MAX_LEVEL = 11;
+
 export const getColor = value => {
   let num = 255;
+  const numVal = Number(value);
 
-  if (Number(value) >= 0 && Number(value) < 20) {
+  num = Math.floor((MAX_COLOR / MAX_LEVEL) * 10 + 5);
+
+  if (numVal >= 0 && numVal < 20) {
     num = 255;
-  } else if (Number(value) >= 20 && Number(value) < 30) {
-    num = Math.ceil((255 / 11) * 10 + 5);
-  } else if (Number(value) >= 30 && Number(value) < 40) {
-    num = Math.ceil((255 / 11) * 10);
-  } else if (Number(value) >= 40 && Number(value) < 50) {
-    num = Math.ceil((255 / 11) * 9);
-  } else if (Number(value) >= 50 && Number(value) < 60) {
-    num = Math.ceil((255 / 11) * 8);
-  } else if (Number(value) >= 60 && Number(value) < 70) {
-    num = Math.ceil((255 / 11) * 7);
-  } else if (Number(value) >= 70 && Number(value) < 80) {
-    num = Math.ceil((255 / 11) * 6);
-  } else if (Number(value) >= 80 && Number(value) < 100) {
-    num = Math.ceil((255 / 11) * 4);
-  } else if (Number(value) >= 100 && Number(value) < 120) {
-    num = (255 / 11) * 3;
-  } else if (Number(value) >= 121 && Number(value) < 150) {
-    num = (255 / 11) * 2;
+  } else if (numVal >= 20 && numVal < 30) {
+    num = 237;
+  } else if (numVal >= 30 && numVal < 40) {
+    num = 232;
+  } else if (numVal >= 40 && numVal < 50) {
+    num = 209;
+  } else if (numVal >= 50 && numVal < 60) {
+    num = 186;
+  } else if (numVal >= 60 && numVal < 70) {
+    num = 163;
+  } else if (numVal >= 70 && numVal < 80) {
+    num = 140;
+  } else if (numVal >= 80 && numVal < 100) {
+    num = 93;
+  } else if (numVal >= 100 && numVal < 120) {
+    num = 70;
+  } else if (numVal >= 121 && numVal < 150) {
+    num = 47;
   } else {
     num = 0;
   }
 
-  // Math.
-
-  // const num = Math.min(255 - Number(value), 255);
   const hex = num.toString(16);
 
   return `#${hex}${hex}${hex}`;
@@ -35,23 +38,23 @@ export const getColor = value => {
 
 export const getOpacity = value => {
   let num = 0.1;
+  const numVal = Number(value);
 
-  if (Number(value) >= 0 && Number(value) < 20) {
+  if (numVal >= 0 && numVal < 20) {
     num = 0.05;
-  } else if (Number(value) >= 20 && Number(value) < 30) {
+  } else if (numVal >= 20 && numVal < 30) {
     num = 0.1;
-  } else if (Number(value) >= 30 && Number(value) < 40) {
+  } else if (numVal >= 30 && numVal < 40) {
     num = 0.4;
-  } else if (Number(value) >= 40 && Number(value) < 50) {
+  } else if (numVal >= 40 && numVal < 50) {
     num = 0.65;
-  } else if (Number(value) >= 50 && Number(value) < 60) {
+  } else if (numVal >= 50 && numVal < 60) {
     num = 0.8;
-  } else if (Number(value) >= 60 && Number(value) < 70) {
+  } else if (numVal >= 60 && numVal < 70) {
     num = 0.9;
   } else {
     num = 1.0;
   }
 
-  // return Number(value) / 255 + 0.6;
   return num;
 };
