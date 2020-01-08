f771eba94589c0ab4bb9aed1eec0c5d78ad9d903
ifjso <ojsboy@hotmail.com>
2020-01-08 19:33:28 +0900

    시도 미세먼지 api 추가

--- src/modules/api/airkorea-api.js
+++ src/modules/api/airkorea-api.js
@@ -1,14 +1,23 @@
 import axios from 'axios';
 
-const AIRKOREA_MICRODUST_URI =
-  '/openapi/services/rest/ArpltnInforInqireSvc/getCtprvnMesureLIst';
+const AIRKOREA_MICRODUST_URI = '/openapi/services/rest/ArpltnInforInqireSvc/getCtprvnMesureLIst';
+
+const AIRKOREA_MICRODUST_SIDO_URI =
+  '/openapi/services/rest/ArpltnInforInqireSvc/getCtprvnMesureSidoLIst';
 
 const AIRKOREA_API_KEY = 'apikey';
 
 export const getMicrodustsByCity = async () => {
   const data = await axios.get(
-    `${AIRKOREA_MICRODUST_URI}?itemCode=PM10&dataGubun=HOUR&pageNo=1&numOfRows=50&_returnType=json&ServiceKey=${AIRKOREA_API_KEY}`,
-    { crossdomain: true }
+    `${AIRKOREA_MICRODUST_URI}?itemCode=PM10&dataGubun=HOUR&pageNo=1&numOfRows=50&_returnType=json&ServiceKey=${AIRKOREA_API_KEY}`
+  );
+
+  return data.data.list;
+};
+
+export const getMircordustsBySido = async () => {
+  const data = await axios.get(
+    `${AIRKOREA_MICRODUST_SIDO_URI}?sidoName=서울&searchCondition=HOUR&pageNo=1&numOfRows=50&_returnType=json&ServiceKey=${AIRKOREA_API_KEY}`
   );
 
   return data.data.list;
